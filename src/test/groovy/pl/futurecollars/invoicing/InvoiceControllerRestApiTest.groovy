680f0453951f8c5d4159b238202df4b1687ebb94
KacperKropidlowski <105714095+KacperKropidlowski@users.noreply.github.com>
2022-09-23 20:59:01 +0200

    Lesson 6 (#8)
    * Lesson 3 Task 1
    
    * Lesson 3 Task 1 update
    
    * Lesson 3 Task 1 update
    
    * Lesson 4 Spring and REST Controller
    
    * Lesson 4 Spring and REST Controller
    
    * code refactoring
    
    * code refactoring
    
    * Lesson 4 Spring and REST Controller update
    
    * Lesson 5 Dependencies injection
    
    * Lesson 5 correction
    
    * Lesson 5 correction
    
    * Task1
    
    * Lesson 6 Testing REST Services
    
    * Lesson 6 Testing REST Services correction
    
    * Lesson 6 Testing REST Services correction 2.0

---
+++ src/test/groovy/pl/futurecollars/invoicing/InvoiceControllerRestApiTest.groovy
@@ -0,0 +1,116 @@
+package pl.futurecollars.invoicing
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import pl.futurecollars.invoicing.db.file.JsonService
+import spock.lang.Specification
+import spock.lang.Stepwise
+
+@Stepwise
+@AutoConfigureMockMvc
+@SpringBootTest
+class InvoiceControllerRestApiTest extends Specification {
+
+    @Autowired
+    JsonService jsonService
+
+    @Autowired
+    MockMvc mockMvc
+
+    def "should try to get invoice from empty database and get status 404"() {
+        expect:
+        mockMvc.perform(MockMvcRequestBuilders.get("/invoices/100"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn().response.contentAsString == ""
+    }
+
+    def "should try to delete invoice from empty database and get status 404"() {
+        expect:
+        mockMvc.perform(MockMvcRequestBuilders.delete("/invoices/100"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn().response.contentAsString == ""
+    }
+
+    def "should try to update invoice from empty database and get status 404"() {
+        expect:
+        mockMvc.perform(MockMvcRequestBuilders.put("/invoices/100").content(getInvoiceAsString()).contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+    }
+
+    def "should save invoice and return status 200"() {
+        expect:
+        mockMvc.perform(MockMvcRequestBuilders.post("/invoices").content(getInvoiceAsString()).contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().response.contentAsString == "1"
+    }
+
+    def "should get invoice from database and return status 200"() {
+        expect:
+        mockMvc.perform(MockMvcRequestBuilders.get("/invoices/1"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().response.contentAsString == getInvoiceAsString().replace('"id":0', '"id":1')
+    }
+
+    def "should update invoice from database and return status 200"() {
+        given:
+        mockMvc.perform(MockMvcRequestBuilders.get("/invoices/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+        when:
+        mockMvc.perform(MockMvcRequestBuilders.put("/invoices/1").content(getUpdatedInvoiceAsString()).contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+        then:
+        mockMvc.perform(MockMvcRequestBuilders.get("/invoices/1"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().response.contentAsString == getUpdatedInvoiceAsString().replace('"id":0', '"id":1')
+    }
+
+    def "should get all invoices from database and return status 200"() {
+        setup:
+        mockMvc.perform(MockMvcRequestBuilders.post("/invoices").content(getInvoiceAsString()).contentType(MediaType.APPLICATION_JSON))
+        expect:
+        mockMvc.perform(MockMvcRequestBuilders.get("/invoices/all"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().response.contentAsString == getAllInvoicesAsString()
+    }
+
+    def "should delete invoice from database and return status 200"() {
+        given:
+        mockMvc.perform(MockMvcRequestBuilders.get("/invoices/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+        when:
+        mockMvc.perform(MockMvcRequestBuilders.delete("/invoices/1"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+        then:
+        mockMvc.perform(MockMvcRequestBuilders.delete("/invoices/1"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+    }
+
+    def getInvoiceAsString() {
+        return jsonService.convertToJson(DataForTesting.invoice)
+    }
+
+    def getUpdatedInvoiceAsString() {
+        return jsonService.convertToJson(DataForTesting.updatedInvoice)
+    }
+
+    def getAllInvoicesAsString() {
+        return "[" + (getUpdatedInvoiceAsString().replace('"id":0', '"id":1') + "," + getInvoiceAsString().replace('"id":0', '"id":2')) + "]"
+    }
+
+}
